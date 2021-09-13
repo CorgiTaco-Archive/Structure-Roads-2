ff6c99bbbfd3e5ee731c1cc317ceeb5d863dece1
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-09-13 13:23:15 -0700

    Switch to longs

--- src/main/java/corgitaco/modid/world/path/PathfindingPathGenerator.java
+++ src/main/java/corgitaco/modid/world/path/PathfindingPathGenerator.java
@@ -1,38 +1,48 @@
 package corgitaco.modid.world.path;
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongIterable;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.SectionPos;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeManager;
 import net.minecraft.world.biome.provider.BiomeProvider;
 import net.minecraft.world.server.ServerWorld;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
 
-public class PathfindingPathGenerator implements IPathGenerator{
+public class PathfindingPathGenerator implements IPathGenerator {
     private final Long2ObjectArrayMap<List<BlockPos>> nodesByChunk = new Long2ObjectArrayMap<>();
     private final Long2ObjectArrayMap<List<BlockPos>> lightsByChunk = new Long2ObjectArrayMap<>();
     private final BiomeProvider biomeSource;
     private int nSamples = 0;
+    private static final boolean ADDITIONAL_DEBUG_DETAILS = false;
 
-    public PathfindingPathGenerator(ServerWorld world, BlockPos startPos, BlockPos endPos, Random random, BiomeProvider biomeSource){
+    public PathfindingPathGenerator(ServerWorld world, BlockPos startPos, BlockPos endPos, Random random, BiomeProvider biomeSource) {
         this.biomeSource = biomeSource;
 
-        System.out.println("Started Pathfinder Gen");
+        if (ADDITIONAL_DEBUG_DETAILS) {
+            System.out.println(String.format("Started Pathfinder Gen for: %s - %s", startPos, endPos));
+        }
         long startTime = System.currentTimeMillis();
-        ChunkPos startChunk = new ChunkPos(startPos);
-        ChunkPos endChunk = new ChunkPos(endPos);
+        long startChunk = getChunkLongFromBlockPos(startPos);
+        long endChunk = getChunkLongFromBlockPos(endPos);
 
-        if(startChunk.equals(endChunk)){
-            System.out.println("Rejected path (Were in same chunk)");
+        if (startChunk == endChunk) {
+            if (ADDITIONAL_DEBUG_DETAILS) {
+                System.out.println("Rejected path (Were in same chunk)");
+            }
             return;
         }
 
 
-        Map<Long, Tile> tiles = new HashMap<>();
+        Long2ObjectArrayMap<Tile> tiles = new Long2ObjectArrayMap<>();
 
         PriorityQueue<Tile> tilesToCheck = new PriorityQueue<>(Tile::compareTo);
         Tile tileAtStartPos = new Tile(false, startChunk);
@@ -42,23 +52,23 @@ public class PathfindingPathGenerator implements IPathGenerator{
         tilesToCheck.add(tileAtStartPos);
 
         boolean found = false;
-        while(!tilesToCheck.isEmpty()){
-            if(nSamples > 10000) break; //To not get stuck if goal is completely inaccesible
+        while (!tilesToCheck.isEmpty()) {
+            if (nSamples > 10000) break; //To not get stuck if goal is completely inaccesible
             Tile currentTile = tilesToCheck.poll();
             int newDist = currentTile.distFromStart + 1;
 
-            if(currentTile.pos.equals(endChunk)){
+            if (currentTile.pos == endChunk) {
                 found = true;
                 break;
             }
 
-            for(ChunkPos pos : surroundingChunks(currentTile.pos)){
-                Tile tile = getTileAt(world, pos, tiles);
-                if(tile.isMountain) continue;
+            for (long pos : surroundingChunks(currentTile.pos)) {
+                Tile tile = getTileAt(pos, tiles);
+                if (tile.isMountain) continue;
 
 
-                if(newDist < tile.distFromStart){
-                    if(!tile.active){
+                if (newDist < tile.distFromStart) {
+                    if (!tile.active) {
                         tile.activate(endChunk, newDist);
                     }
 
@@ -70,63 +80,67 @@ public class PathfindingPathGenerator implements IPathGenerator{
             }
         }
 
-        if(found){
-            Tile currentTile = tiles.get(endChunk.toLong());
-            while(currentTile.bestPrev != null){
-                BlockPos start = new BlockPos(currentTile.pos.x * 16 + 8, 60, currentTile.pos.z * 16 + 8);
-                BlockPos end = new BlockPos(currentTile.bestPrev.pos.x * 16 + 8, 60, currentTile.bestPrev.pos.z * 16 + 8);
-                for(float t = 0; t < 1; t += 0.2){
+        if (found) {
+            Tile currentTile = tiles.get(endChunk);
+            while (currentTile.bestPrev != null) {
+                BlockPos start = new BlockPos(ChunkPos.getX(currentTile.pos) * 16 + 8, 60, ChunkPos.getZ(currentTile.pos) * 16 + 8);
+                BlockPos end = new BlockPos(ChunkPos.getX(currentTile.bestPrev.pos) * 16 + 8, 60, ChunkPos.getZ(currentTile.bestPrev.pos) * 16 + 8);
+                for (float t = 0; t < 1; t += 0.2) {
                     addBlockPos(PathGenerator.getLerpedBlockPos(start, end, t));
                 }
 
                 currentTile = currentTile.bestPrev;
             }
-        }else{
-            System.out.println("No path found!");
+        } else {
+            if (ADDITIONAL_DEBUG_DETAILS) {
+                System.out.println("No path found!");
+            }
         }
 
-        System.out.println("Created path in " + (System.currentTimeMillis() - startTime) + " ms");
-        System.out.println("Sampled " + nSamples + " times");
-        System.out.println("Completed pathfinder gen");
+
+        System.out.println(String.format("Pathfinder Gen for: %s - %s took %sms. Sampled %s times.", startPos, endPos, System.currentTimeMillis() - startTime, nSamples));
     }
 
-    private void addBlockPos(BlockPos pos){
-        ChunkPos chunkPos = new ChunkPos(pos);
-        List<BlockPos> nodeList = nodesByChunk.get(chunkPos.toLong());
-        if(nodeList == null){
+    private void addBlockPos(BlockPos pos) {
+        long chunkPos = getChunkLongFromBlockPos(pos);
+        List<BlockPos> nodeList = nodesByChunk.get(chunkPos);
+        if (nodeList == null) {
             nodeList = new ArrayList<>();
-            nodesByChunk.put(chunkPos.toLong(), nodeList);
+            nodesByChunk.put(chunkPos, nodeList);
         }
 
         nodeList.add(pos);
     }
 
-    private Iterable<? extends ChunkPos> surroundingChunks(ChunkPos pos) {
-        List<ChunkPos> points = new ArrayList<>();
-        points.add(new ChunkPos(pos.x + 1, pos.z));
-        points.add(new ChunkPos(pos.x - 1, pos.z));
-        points.add(new ChunkPos(pos.x, pos.z + 1));
-        points.add(new ChunkPos(pos.x, pos.z - 1));
+    private LongIterable surroundingChunks(long pos) {
+        LongArrayList points = new LongArrayList();
+        int x = ChunkPos.getX(pos);
+        int z = ChunkPos.getZ(pos);
+
+        points.add(ChunkPos.asLong(x + 1, z));
+        points.add(ChunkPos.asLong(x - 1, z));
+        points.add(ChunkPos.asLong(x, z + 1));
+        points.add(ChunkPos.asLong(x, z - 1));
 
         return points;
     }
 
-    private Tile getTileAt(ServerWorld world, ChunkPos pos, Map<Long, Tile> tiles){
-        Tile tile = tiles.get(pos.toLong());
-        if(tile == null) {
-            Tile newTile = new Tile(shouldAvoid(world, pos), pos);
-            tiles.put(pos.toLong(), newTile);
+    private Tile getTileAt(long pos, Long2ObjectArrayMap<Tile> tiles) {
+        Tile tile = tiles.get(pos);
+        if (tile == null) {
+            Tile newTile = new Tile(shouldAvoid(pos), pos);
+            tiles.put(pos, newTile);
             return newTile;
         }
         return tile;
     }
 
-    private boolean shouldAvoid(ServerWorld world, ChunkPos pos){
+    private boolean shouldAvoid(long pos) {
         nSamples++;
         //world.getBiome() seems to sometimes get the biome from the world and cause a lock for some reason
         //As far as I can tell this alternative will always calculate the biome
-        Biome biome = world.getBiomeManager().getNoiseBiomeAtPosition(pos.x * 16 + 8, 60, pos.z * 16 + 8);
-        if(biome.getBiomeCategory().equals(Biome.Category.OCEAN))
+        Biome biome = this.biomeSource.getNoiseBiome((ChunkPos.getX(pos) << 2) + 2, 60, (ChunkPos.getZ(pos) << 2) + 2);
+        if (biome.getBiomeCategory().equals(Biome.Category.OCEAN))
             return true;
         return biome.getDepth() > 0.5f;
 
@@ -147,20 +161,20 @@ public class PathfindingPathGenerator implements IPathGenerator{
         return Blocks.RED_CONCRETE.defaultBlockState();
     }
 
-    public static class Tile implements Comparable{
+    public static class Tile implements Comparable {
         private Tile bestPrev = null;
         private int distFromStart = Integer.MAX_VALUE;
         private float distFromGoal = Float.MAX_VALUE;
         private boolean active = false;
         private final boolean isMountain;
-        private final ChunkPos pos;
+        private final long pos;
 
-        public Tile(boolean isMountain, ChunkPos pos){
+        public Tile(boolean isMountain, long pos) {
             this.isMountain = isMountain;
             this.pos = pos;
         }
 
-        public float getFCost(){
+        public float getFCost() {
             return distFromGoal + distFromStart;
         }
 
@@ -168,18 +182,25 @@ public class PathfindingPathGenerator implements IPathGenerator{
         public int compareTo(Object o) {
             Tile tile = (Tile) o;
             int diff = Float.compare(getFCost(), tile.getFCost());
-            if(diff == 0){
+            if (diff == 0) {
                 return Float.compare(distFromGoal, tile.distFromGoal);
             }
             return diff;
         }
 
-        public void activate(ChunkPos goal, int distFromStart){
+        public void activate(long goal, int distFromStart) {
             active = true;
-            int dx = pos.x - goal.x;
-            int dz = pos.z - goal.z;
+            int x = ChunkPos.getX(pos);
+            int z = ChunkPos.getZ(pos);
+
+            int dx = x - ChunkPos.getX(goal);
+            int dz = z - ChunkPos.getZ(goal);
             distFromGoal = (float) Math.sqrt(dx * dx + dz * dz);
             this.distFromStart = distFromStart;
         }
+    }
+
+    public static long getChunkLongFromBlockPos(BlockPos pos) {
+        return ChunkPos.asLong(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
     }
 }
